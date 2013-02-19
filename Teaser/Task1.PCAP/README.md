84c0c933a13e5afedbdf57829937d66648ae38ed
Messiah <m.muzafarov@gmail.com>
2013-02-19 14:49:07 +0600

    icmp_echo

---
+++ Teaser/Task1.PCAP/README.md
@@ -0,0 +1,3 @@
+Trash + trash + icmp with last byte.
+
+Text will be here
---
+++ Teaser/Task1.PCAP/icmp_echo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ -z $1 ] || [ -z $2 ]; then
+    echo "Usage $0 <address> <message>"
+    exit 1
+fi;
+message=$(echo $2 | fold -w1)
+for c in $message; do
+    hping2 -1 -c 1 -e "$c" $1 2> /dev/null
+done
